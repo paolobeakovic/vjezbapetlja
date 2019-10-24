5c380ab28835188b941253966e2f037488debc4a
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2019-10-24 11:32:47 +0200

    Add files via upload

---
+++ beakovicPaoloVjezbaPetlje/beakovicPaoloVjezbaPetlje/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beakovicPaoloVjezbaPetlje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unosTeksta;
+
+            Console.WriteLine("Unesi tekst: ");
+
+            unosTeksta = Console.ReadLine();
+
+            Console.WriteLine(unosTeksta);
+
+            for (int i = unosTeksta.Length; i > 0; i--)
+            {
+                Console.WriteLine(unosTeksta.Substring(0, i - 1));
+            }
+            Console.ReadKey();
+        }
+    }
+}
